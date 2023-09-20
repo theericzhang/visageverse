d2889980cd10cba78def2c9390a58ed31724b1a2
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-09-20 14:12:33 -0400

    lets fucking go webcam working

--- src/app/components/WebcamComponent.tsx
+++ src/app/components/WebcamComponent.tsx
@@ -1,5 +1,29 @@
-import Webcam from "react-webcam";
+"use client";
+import React, { useEffect, useRef } from "react";
 
-export default function WebcamComponent() {
-    return <Webcam />;
-}
+const WebcamComponent = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        // Function to start the webcam
+        const startWebcam = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: true,
+                });
+
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (error) {
+                console.error("Error accessing webcam:", error);
+            }
+        };
+
+        startWebcam();
+    }, []);
+
+    return <video ref={videoRef} autoPlay playsInline />;
+};
+
+export default WebcamComponent;

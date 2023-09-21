ec687b5d8e3e08868c4cee6cf9d4695d039b354f
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-09-21 11:57:20 -0400

    think I got it? add webpack configs to turn off fs and encoding fallback

--- src/app/components/WebcamComponent/WebcamComponent.tsx
+++ src/app/components/WebcamComponent/WebcamComponent.tsx
@@ -1,17 +1,23 @@
 "use client";
 import React, { useEffect, useRef } from "react";
-import {
-    TinyFaceDetector,
-    FaceLandmark68Net,
-    FaceRecognitionNet,
-    FaceExpressionNet,
-} from "face-api.js";
+import * as faceapi from "face-api.js";
 
 const WebcamComponent = () => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
 
     useEffect(() => {
-        // Function to start the webcam
+        // Function to start the webcam and perform face recognition
+        const loadAllModels = () => {
+            Promise.all([
+                faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+                faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+                faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+            ])
+                .then(() => console.log("done"))
+                .catch((e) => console.log(e));
+        };
+
+        videoRef && loadAllModels();
         const startWebcam = async () => {
             try {
                 const stream = await navigator.mediaDevices.getUserMedia({
@@ -20,6 +26,43 @@ const WebcamComponent = () => {
 
                 if (videoRef.current) {
                     videoRef.current.srcObject = stream;
+
+                    // Initialize faceapi with the video element
+                    const canvas = faceapi.createCanvasFromMedia(
+                        videoRef.current
+                    );
+                    document.body.append(canvas);
+
+                    // Set canvas dimensions
+                    canvas.width = videoRef.current.width || 640;
+                    canvas.height = videoRef.current.height || 480;
+
+                    // Perform face detection and recognition
+                    const displaySize = {
+                        width: canvas.width,
+                        height: canvas.height,
+                    };
+                    faceapi.matchDimensions(canvas, displaySize);
+
+                    // Run face detection and landmarks detection
+                    const detections = await faceapi
+                        .detectAllFaces(
+                            videoRef.current,
+                            new faceapi.TinyFaceDetectorOptions()
+                        )
+                        .withFaceLandmarks()
+                        .withFaceDescriptors();
+                    const resizedDetections = faceapi.resizeResults(
+                        detections,
+                        displaySize
+                    );
+
+                    // Clear the canvas and draw the results
+                    canvas
+                        ?.getContext("2d")
+                        ?.clearRect(0, 0, canvas.width, canvas.height);
+                    faceapi.draw.drawDetections(canvas, resizedDetections);
+                    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
                 }
             } catch (error) {
                 console.error("Error accessing webcam:", error);

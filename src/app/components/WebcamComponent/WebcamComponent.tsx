ed376f9bd6c957338c06b9a6a54d94c40934fb70
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-09-22 16:38:27 -0400

    remove old startWebcam function

--- src/app/components/WebcamComponent/WebcamComponent.tsx
+++ src/app/components/WebcamComponent/WebcamComponent.tsx
@@ -79,56 +79,6 @@ const WebcamComponent = () => {
         }
     };
 
-    // const startWebcam = async () => {
-    //     try {
-    //         const stream = await navigator.mediaDevices.getUserMedia({
-    //             video: true,
-    //         });
-
-    //         if (videoRef.current) {
-    //             console.log("expect load");
-    //             videoRef.current.srcObject = stream;
-
-    //             // Initialize faceapi with the video element
-    //             const canvas = faceapi.createCanvasFromMedia(videoRef.current);
-    //             document.body.append(canvas);
-
-    //             // Set canvas dimensions
-    //             canvas.width = videoRef.current.width || 640;
-    //             canvas.height = videoRef.current.height || 480;
-
-    //             // Perform face detection and recognition
-    //             const displaySize = {
-    //                 width: canvas.width,
-    //                 height: canvas.height,
-    //             };
-    //             faceapi.matchDimensions(canvas, displaySize);
-
-    //             // Run face detection and landmarks detection
-    //             const detections = await faceapi
-    //                 .detectAllFaces(
-    //                     videoRef.current,
-    //                     new faceapi.TinyFaceDetectorOptions()
-    //                 )
-    //                 .withFaceLandmarks()
-    //                 .withFaceDescriptors();
-    //             const resizedDetections = faceapi.resizeResults(
-    //                 detections,
-    //                 displaySize
-    //             );
-
-    //             // Clear the canvas and draw the results
-    //             canvas
-    //                 ?.getContext("2d")
-    //                 ?.clearRect(0, 0, canvas.width, canvas.height);
-    //             faceapi.draw.drawDetections(canvas, resizedDetections);
-    //             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    //         }
-    //     } catch (error) {
-    //         console.error("Error accessing webcam:", error);
-    //     }
-    // };
-
     useEffect(() => {
         videoRef && loadAllModels();
 
